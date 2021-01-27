6d22cd2a4082750e54e991469cf1287409408324
Will-Gon <will.colocho@gmail.com>
2021-01-27 09:32:32 -0800

    need to restart computer

--- src/Containers/Bikes.js
+++ src/Containers/Bikes.js
@@ -3,22 +3,49 @@ import Harley1 from '../SlideShowImages/Harley1.jpg'
 import Harley2 from '../SlideShowImages/Harley2.jpg'
 import Harley3 from '../SlideShowImages/Harley3.jpg'
 import Harley4 from '../SlideShowImages/Harley4.jpg'
+import Harley5 from '../MotorcycleImages/Harley5.jpeg'
+import Harley6 from '../MotorcycleImages/Harley6.jpeg'
+import Gsxr7 from '../MotorcycleImages/Gsxr7.jpeg'
+import Gsxr8 from '../MotorcycleImages/Gsxr8.jpeg'
+import Gsxr9 from '../MotorcycleImages/Gsxr9.jpeg'
+import Gsxr10 from '../MotorcycleImages/Gsxr10.jpeg'
+
 
 const Bikes = () => {
     return (
         <div>
             <h1>Bikes Page</h1>
-            <div className='bike-slides'>
-                <img src={Harley1} alt='Bike Club' />
-            </div>
-            <div className='bike-slides'>
-                <img src={Harley2} alt='Bike Club'/>
-            </div>
-            <div className='bike-slides'>
-                <img src={Harley3} alt='Bike Club'/>
-            </div>
-            <div className='bike-slides'>
-                <img src={Harley4} alt='Bike Club'/>
+            <div className='place-meets-wrapper'>
+                <div className='bay-area-meets'>
+                    <img src={Harley1} alt='Bike Club' />
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Harley2} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Harley3} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Harley4} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Harley5} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Harley6} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Gsxr7} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Gsxr8} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Gsxr9} alt='Bike Club'/>
+                </div>
+                <div className='bay-area-meets'>
+                    <img src={Gsxr10} alt='Bike Club'/>
+                </div>
             </div>
         </div>
         
--- src/Containers/FavoritesContainer.js
+++ src/Containers/FavoritesContainer.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class FavoritesContainer extends Component {
     addFaves = () => {
         return this.props.favorites.map((fav) => (
-            //console.log(fav.place.image_url)
+            //console.log(fav.place.name)
             <div className='bay-area-meets'>
-                <a className='navlink' href={fav.place.name}>
                 <img src={fav.place.image_url} alt='Place'/>
-                </a>
+                <h2>{fav.place.name}</h2>
+                <p>{fav.place.description}</p>
             </div>
         ))
     }
